2dece483a23a1b95469e8f90e738ab278da0e1d3
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-05-06 09:26:41 +0530

    Add files via upload

---
+++ prac7/module7.py
@@ -0,0 +1,9 @@
+def area(r):
+	from math import pi as k
+	area = k*r*r
+	print("Area of circle is ",area)
+
+def circum(r):
+	from math import pi as k
+	circum = 2*k*r
+	print("Cicumeference of circle is ",circum)
---
+++ prac7/prac7.py
@@ -0,0 +1,6 @@
+r = int(input("Enter a radius for find area and cicumference of circle "))
+
+from module7 import circum,area
+print("\n")
+circum(r)
+area(r)
