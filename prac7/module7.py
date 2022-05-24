803b1b694841a81f9c7b98a8a44d52b1f5a3a2e4
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-05-24 07:04:49 +0530

    Delete prac7 directory

--- prac7/module7.py
+++
@@ -1,9 +0,0 @@
-def area(r):
-	from math import pi as k
-	area = k*r*r
-	print("Area of circle is ",area)
-
-def circum(r):
-	from math import pi as k
-	circum = 2*k*r
-	print("Cicumeference of circle is ",circum)
