d9e67561842b2be6d70eb6cfaed8616b5948b7a6
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-05-29 01:56:20 +0800

    Update practical 10.py

--- practical 10.py
+++ practical 10.py
@@ -1,11 +1,12 @@
 # practical 10
+# Write a program to calculate the sum of elements within a list.Catch all necessarry exceptions.
 
 try:
-    n = int(input("Enter a number of number: "))
+    n = int(input("Enter a number of element of list: "))
     s = []
 
     for j in  range(n):
-        a = int(input("Enter a number for sum: "))
+        a = int(input("Enter a number for sum : "))
         s.append(a)
 
     print(sum(s))
