0f34a81711abda0a2affa4dd9160426dc2c178b8
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-05-29 01:48:50 +0800

    Update practical 9.py

--- practical 9.py
+++ practical 9.py
@@ -1,11 +1,15 @@
+# practical 9
+# Write a user-defined exception that could be raised when the text entered by user consists of less than 10 characters.
+
 try:
     a = input("Enter your characters: ")
-    l = len(a)
 
-    if l <= 10:
-        raise Exception("Please Enter more than 10 digit.") 
+    if len(a) <= 10:
+        raise Exception("Please Enter more than 10 characters.")
 
-
-
+        
 except Exception as d:
     print(d)
+
+finally:
+    print("Finished")
