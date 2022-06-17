76e50885ec1cc9014b48a47c03f847d116be7a5e
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-06-17 14:51:16 +0530

    Update practical 18.py

--- practical 18.py
+++ practical 18.py
@@ -2,22 +2,22 @@ dict1 = {}
 dict2 = {}
 dict3 = {}
 
+
 def inputs(dictionary):
     n = int(input("Enter a total number of key for dictionary: "))
     for j in range(n):
         a = input("Enter key and value with space for dictionary: ").split()
         dictionary[a[0]] = a[1] 
 
+def its(maindict,finaldict):
+    for t in maindict:
+        finaldict[t] = maindict[t]
+
+
 inputs(dict1)
-inputs(dict2)    
+inputs(dict2)  
 
-
-def loops(maindict,finaldict):
-    for tt in maindict:
-        for t in maindict.values():
-            finaldict[tt] = t
-            
-loops(dict1,dict3)
-loops(dict2,dict3)
-        
-print(dict3)
+its(dict1,dict3)
+its(dict2,dict3)
+       
+print("Your final dictionary is: ",dict3)
