f7f24237937085bc596a95507cfe3217875b6bde
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-06-17 14:06:42 +0530

    Update practical 18.py

--- practical 18.py
+++ practical 18.py
@@ -1,28 +1,23 @@
-# practical 18
-# write a program to join two given dictionary to creat new one.
-
 dict1 = {}
 dict2 = {}
 dict3 = {}
 
-n = int(input("Enter a total number of key for dictionary: "))
-for j in range(n):
-    a = input("Enter key and value with space for dictionary: ").split()
-    dict1[a[0]] = a[1] 
-    
+def inputs(dictionary):
+    n = int(input("Enter a total number of key for dictionary: "))
+    for j in range(n):
+        a = input("Enter key and value with space for dictionary: ").split()
+        dictionary[a[0]] = a[1] 
 
-n = int(input("Enter a total number of key for dictionary: "))
-for j in range(n):
-    a = input("Enter key and value with space for dictionary: ").split()
-    dict2[a[0]] = a[1] 
+inputs(dict1)
+inputs(dict2)    
 
 
-for tt in dict1:
-    for t in dict1.values():
-        dict3[tt] = t
+def loops(maindict,finaldict):
+    for tt in maindict:
+        for t in maindict.values():
+            finaldict[tt] = t
+            
+loops(dict1,dict3)
+loops(dict2,dict3)
         
-for tt in dict2:
-    for t in dict2.values():
-        dict3[tt] = t
-        
-dict3
+print(dict3)
