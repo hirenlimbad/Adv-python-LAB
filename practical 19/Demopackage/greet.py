67eae92ac2dc8163c0182388671a3de6c866aa95
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-06-21 08:05:21 +0530

    Add files via upload

---
+++ practical 19/Demopackage/greet.py
@@ -0,0 +1,5 @@
+def sayhello():
+	print("Hello dear!")
+
+def sayhiito(a):
+	print("Hello",a)
---
+++ practical 19/main.py
@@ -0,0 +1,7 @@
+from  Demopackage import greet as d
+from  Demopackage import module1 as b
+
+d.sayhello()
+c = b.sum1(2,3)
+
+print(c)
