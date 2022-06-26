bac8850f22c380dfc0696df7b3ee885a25e4fcbc
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-06-26 17:08:30 +0530

    Delete practical 19 directory

--- practical 19/Demopackage/greet.py
+++
@@ -1,5 +0,0 @@
-def sayhello():
-	print("Hello dear!")
-
-def sayhiito(a):
-	print("Hello",a)
--- practical 19/main.py
+++
@@ -1,9 +0,0 @@
-from  Demopackage import greet as d
-from  Demopackage import module1 as b
-
-d.sayhello()
-c = b.sum1(2,3)
-
-print(c)
-
-# more arguments and function you can call with check module
