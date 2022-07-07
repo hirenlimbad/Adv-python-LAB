537d6fbab859ec6a19b25e00232a01c4dedcee15
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-07-07 21:58:00 +0530

    Add files via upload

---
+++ practical 23.py
@@ -0,0 +1,26 @@
+#practical 23
+#Write a program that takes input from a text file. The program should print all of the unique words in the file in alphabetical order.
+fp = open('data.txt','w')
+a = input('Enter your data for text file: ')
+fp.write(a)
+fp.close()
+
+
+fp1 = open('data.txt','r')
+n = 1
+l1 = []
+while True:
+	line = fp1.readline().split()
+
+	if not line:
+		break
+
+	for j in line:
+		l1.append(j)
+
+l1.sort()
+fp1.close()
+
+print('Below printed every word in alphabetical order...')
+for b in l1:
+	print(b)
