d5cab8fc49dc15b6d24f494eaf8b5d85969d4ddd
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2022-07-28 10:07:24 +0530

    Update practical 18.py

--- practical 18.py
+++ practical 18.py
@@ -1,3 +1,6 @@
+# Write a program to concatenate the following dictionaries to create a new one.
+# Sample Dictionary: dic1={1:10, 2:20} dic2={3:30, 4:40} dic3={5:50,6:60} Expected Result: {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
 dict1 = {}
 dict2 = {}
 dict3 = {}
